12c3a9cec142d739d9798aa04b93b72569356a53
naveen59835 <naveenkumar59835@gmail.com>
2022-11-30 19:47:30 +0530

    Created instance variables constructor getters and setters, tostring

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -7,4 +7,50 @@
 package com.niit.jdp.model;
 
 public class Playlist {
+    int playlistId;
+    String playlistName;
+    int songId;
+
+    public Playlist(int playlistId, String playlistName, int songId) {
+        this.playlistId = playlistId;
+        this.playlistName = playlistName;
+        this.songId = songId;
+    }
+
+    public Playlist() {
+    }
+
+    public int getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(int playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+
+    @Override
+    public String toString() {
+        return "Playlist{" +
+                "playlistId=" + playlistId +
+                ", playlistName='" + playlistName + '\'' +
+                ", songId=" + songId +
+                '}';
+    }
+
 }
