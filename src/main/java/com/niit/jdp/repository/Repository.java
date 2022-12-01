be6d7c5b51833fe2612b2f645bef90a17dfb1edb
naveen59835 <naveenkumar59835@gmail.com>
2022-12-01 11:35:54 +0530

    Created Repository.java

--- src/main/java/com/niit/jdp/repository/Repository.java
+++ src/main/java/com/niit/jdp/repository/Repository.java
@@ -1,10 +1,4 @@
-/*
- * Author : Naveen Kumar
- * Date : 01-12-2022
- * Created With : IntelliJ IDEA Community Edition
- */
-
 package com.niit.jdp.repository;
 
-public class Repository {
+public interface Repository {
 }
