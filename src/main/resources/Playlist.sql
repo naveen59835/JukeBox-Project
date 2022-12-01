4e26c287c84dd4fc30aea9cdafc63b5c7b362f1e
naveen59835 <naveenkumar59835@gmail.com>
2022-12-01 10:17:18 +0530

    inserted values into playlist table in sql

--- src/main/resources/Playlist.sql
+++ src/main/resources/Playlist.sql
@@ -21,3 +21,18 @@ create table `jukebox`.`user`(
 userId varchar(50),
 password varchar(50)
 );
+-- inserted values in the song taable
+insert into jukebox.song value(1,'adiye_azhage','Beiber','pop','6 mins');
+insert into jukebox.song value(2,'minsara','gomez','pop','2 mins');
+insert into jukebox.song value(3,'naan','shakira','treble','3 mins');
+insert into jukebox.song value(4,'pirates','scoopdoo','solo','2 mins');
+insert into jukebox.song value(5,'rolex1','rahmaan','peace','3 mins');
+insert into jukebox.song value(6,'rolex2','rahmaan','solo','3 mins');
+insert into jukebox.song value(7,'varanam','yuvan','boost','4 mins');
+insert into jukebox.song value(8,'vikramvedha','Beiber','rock','1 mins');
+insert into jukebox.song value(9,'yanji','Beiber','bass','3 mins');
+-- insert values into playlist table
+insert into jukebox.playlist value(12,'pop playlist',1);
+insert into jukebox.playlist value(13,'pop playlist',2);
+insert into jukebox.playlist value(14,'fusion playlist',3);
+insert into jukebox.playlist value(15,'fusion playlist',4);
