3dcd65049ab089c3f316967c54acf5b6fc73a954
naveen59835 <naveenkumar59835@gmail.com>
2022-12-01 19:36:00 +0530

    inside the music player class created the file object and passing parameters to it

--- src/main/java/com/niit/jdp/service/MusicPlayerService.java
+++ src/main/java/com/niit/jdp/service/MusicPlayerService.java
@@ -6,5 +6,30 @@
 
 package com.niit.jdp.service;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
 public class MusicPlayerService {
+    public void getSongById(int songId){
+        Scanner scanner=new Scanner(System.in);
+        try {
+            DatabaseConnectionService databaseConnectionService=new DatabaseConnectionService();
+            Connection connection= databaseConnectionService.connection;
+            String playQuery="Select*from`jukebox`.`songs` where songId=?;";
+            PreparedStatement preparedStatement= connection.prepareStatement(playQuery);
+            preparedStatement.setInt(1,songId);
+            ResultSet resultSet=preparedStatement.executeQuery();
+            while (resultSet.next()){
+                File file=new File(resultSet.getString(1));
+
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
