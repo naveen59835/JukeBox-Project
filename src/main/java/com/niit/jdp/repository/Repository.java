65c28df382f398d6ac59ef980b9030cdcc5268a5
naveen59835 <naveenkumar59835@gmail.com>
2022-12-01 11:41:00 +0530

    added interface repository and all methods to it

--- src/main/java/com/niit/jdp/repository/Repository.java
+++ src/main/java/com/niit/jdp/repository/Repository.java
@@ -1,4 +1,13 @@
 package com.niit.jdp.repository;
 
+import com.niit.jdp.model.Playlist;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface Repository {
+    List<Playlist> getAll(Connection connection) throws SQLException;
+    Playlist getBySongId(Connection connection, int songId) throws SQLException;
+    boolean deleteBySongId(Connection connection, int songId) throws SQLException;
 }
